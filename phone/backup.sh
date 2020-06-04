571fb72edc6b4c3fdaeece934d7015dddda0f23c
wdhg <wdhgrant@gmail.com>
2020-06-04 17:45:27 +0100

    fix bug with file names with spaces

--- phone/backup.sh
+++ phone/backup.sh
@@ -1,5 +1,5 @@
 BACKUPS_DIR=~/backups/phone
-TARGET_DIRS=(/sdcard/Snapchat /sdcard/Pictures /sdcard/DCIM)
+TARGET_DIRS=(/sdcard/Snapchat /sdcard/Pictures /sdcard/DCIM /sdcard/Movies /sdcard/Download /sdcard/WhatsApp)
 
 if [ $(adb get-state)  != "device" ]
 then
@@ -11,22 +11,23 @@ echo "Starting phone backup..."
 
 for target_dir in ${TARGET_DIRS[*]}
 do
+  echo ""
   echo "Backing up $target_dir to $BACKUPS_DIR$target_dir..."
+  echo ""
 
-  files=$(adb shell find $target_dir -type f)
-
-  for target_file in ${files[*]}
+  IFS=$'\n' # set for loop delimeter to newline
+  for target_file in $(adb shell find "$target_dir" -type f)
   do
     backup_file="$BACKUPS_DIR$target_file"
     backup_file_dir=$(dirname $backup_file)
     # check if files backup directory exists
-    if [ ! -d $backup_file_dir ];
+    if [ ! -d "$backup_file_dir" ];
     then
-      mkdir -p $backup_file_dir
+      mkdir -p "$backup_file_dir"
     fi
-    if [ ! -f $backup_file ];
+    if [ ! -f "$backup_file" ];
     then
-      adb pull $target_file $backup_file
+      adb pull "$target_file" "$backup_file"
     fi
   done
 done
